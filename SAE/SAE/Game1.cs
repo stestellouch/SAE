8f6304af858bee5a44be79930d187b053462d27c
Evan Lamy <evan.lamy@etu.univ-smb.fr>
2023-01-05 08:49:03 +0100

    Début

--- SAE/SAE/Game1.cs
+++ SAE/SAE/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
 using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
 
@@ -12,6 +15,11 @@ namespace SAE
         private SpriteBatch _spriteBatch;
         private TiledMap _tiledMap;
         private TiledMapRenderer _tiledMapRenderer;
+        private KeyboardState _keyboardState;
+        private float deltaSeconds;
+        private Vector2 _positionMC;
+        private AnimatedSprite _MC;
+        
 
         public Game1()
         {
@@ -24,6 +32,7 @@ namespace SAE
         {
             // TODO: Add your initialization logic here
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
+            _positionMC = new Vector2(0, 0);
 
             base.Initialize();
         }
@@ -35,6 +44,7 @@ namespace SAE
             // TODO: use this.Content to load your game content here
             _tiledMap = Content.Load<TiledMap>("Tile/Test");
             _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            SpriteSheet SpriteMC = Content.Load<SpriteSheet>("Animation/MC.sf", new JsonContentLoader());
         }
 
         protected override void Update(GameTime gameTime)
@@ -44,6 +54,7 @@ namespace SAE
 
             // TODO: Add your update logic here
             _tiledMapRenderer.Update(gameTime);
+            _MC.Update(deltaSeconds);
 
             base.Update(gameTime);
         }
