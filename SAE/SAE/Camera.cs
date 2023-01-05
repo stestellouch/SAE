fa71d0a3c9157632db0682be34aa3919836bf457
Stella Maestre <stella.maestre@etu.univ-smb.fr>
2023-01-05 15:46:27 +0100

    Revert "camera updaqte"
    This reverts commit 2547bf1bde30c84985133835732c01e55e309301.

--- SAE/SAE/Camera.cs
+++ SAE/SAE/Camera.cs
@@ -45,34 +45,5 @@ namespace SAE
             Position = position;
 
         }
-        public Matrix GetViewTransformationMatrix()
-        {
-            if (_isViewTransformationDirty)
-            {
-                _camTranslationVector.X = -_position.X;
-                _camTranslationVector.Y = -_position.Y;
-
-                Matrix.CreateTranslation(ref _camTranslationVector, out _camTranslationMatrix);
-                Matrix.CreateRotationZ(_rotation, out _camRotationMatrix);
-
-                _camScaleVector.X = _zoom;
-                _camScaleVector.Y = _zoom;
-                _camScaleVector.Z = 1;
-
-                Matrix.CreateScale(ref _camScaleVector, out _camScaleMatrix);
-
-                _resTranslationVector.X = ResolutionIndependentRenderer.VirtualWidth * 0.5f;
-                _resTranslationVector.Y = ResolutionIndependentRenderer.VirtualHeight * 0.5f;
-                _resTranslationVector.Z = 0;
-
-                Matrix.CreateTranslation(ref _resTranslationVector, out _resTranslationMatrix);
-
-                _transform = _camTranslationMatrix * _camRotationMatrix * _camScaleMatrix * _resTranslationMatrix * ResolutionIndependentRenderer.GetTransformationMatrix();
-                _isViewTransformationDirty = false;
-
-            }
-            return _transform;
-
-        }
     }
 }
