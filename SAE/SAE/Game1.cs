216d6afe46a5ba6eb80a6f15731c1a8efba3ec16
Evan Lamy <evan.lamy@etu.univ-smb.fr>
2023-01-05 11:45:44 +0100

    Anim droite et gauche

--- SAE/SAE/Game1.cs
+++ SAE/SAE/Game1.cs
@@ -19,11 +19,11 @@ namespace SAE
         private TiledMapRenderer _tiledMapRenderer;
         private KeyboardState _keyboardState;
         private float deltaSeconds;
-        private Vector2 _positionMC;
-        private AnimatedSprite _MC;
-        private int _sensXMC;
-        private int _sensYMC;
-        private int _vitesseMC;
+        private Vector2 _positionPerso;
+        private AnimatedSprite _Perso;
+        private int _sensPersoX;
+        private int _sensPersoY;
+        private int _vitessePerso;
 
 
         //taille écran pour caméra
@@ -42,15 +42,15 @@ namespace SAE
         {
             
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
-            _positionMC = new Vector2(0, 0);
-            _sensYMC = 0;
-            _sensXMC = 0;
-            _vitesseMC = 100;
+            _positionPerso = new Vector2(0, 0);
+            _sensPersoY = 0;
+            _sensPersoX = 0;
+            _vitessePerso = 100;
 
 
             
-            _graphics.PreferredBackBufferHeight = 1920;
-            _graphics.PreferredBackBufferWidth = 1080;
+            _graphics.PreferredBackBufferHeight = 1050;
+            _graphics.PreferredBackBufferWidth = 1680;
             _graphics.ApplyChanges();
 
             base.Initialize();
@@ -64,7 +64,7 @@ namespace SAE
             _tiledMap = Content.Load<TiledMap>("Tile/Test");
             _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
             SpriteSheet SpriteMC = Content.Load<SpriteSheet>("Animation/MC.sf", new JsonContentLoader());
-            _MC = new AnimatedSprite(SpriteMC);
+            _Perso = new AnimatedSprite(SpriteMC);
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
 
@@ -77,55 +77,64 @@ namespace SAE
 
             
             _tiledMapRenderer.Update(gameTime);
-            _MC.Update(deltaSeconds);
+            
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            _Perso.Update(deltaTime);
             _keyboardState = Keyboard.GetState();
             //Si la touche droite est pressé
             if (_keyboardState.IsKeyDown(Keys.Right) && !(_keyboardState.IsKeyDown(Keys.Left)))
             {
-                _sensXMC = 1;
-                _sensYMC = 0;
-                _positionMC.X += _sensXMC * _vitesseMC * deltaTime;
+                _sensPersoX = 1;
+                _positionPerso.X += _sensPersoX * _vitessePerso * deltaTime;
 
             }
             //Si la touche gauche est pressé
             if (_keyboardState.IsKeyDown(Keys.Left) && !(_keyboardState.IsKeyDown(Keys.Right)))
             {
-                _sensXMC = -1;
-                _sensYMC = 0;
-                _positionMC.X += _sensXMC * _vitesseMC * deltaTime;
+                _sensPersoX = -1;
+                _positionPerso.X += _sensPersoX * _vitessePerso * deltaTime;
 
             }
             //Si la touche haut est pressé
             if (_keyboardState.IsKeyDown(Keys.Up) && !(_keyboardState.IsKeyDown(Keys.Down)))
             {
-                _sensYMC = -1;
-                _sensXMC = 0;
-                _positionMC.Y += _sensYMC * _vitesseMC * deltaTime;
+                _sensPersoY = -1;
+                _positionPerso.Y += _sensPersoY * _vitessePerso * deltaTime;
 
             }
             //Si la touche bas est pressé
             if (_keyboardState.IsKeyDown(Keys.Down) && !(_keyboardState.IsKeyDown(Keys.Up)))
             {
-                _sensYMC = 1;
-                _sensXMC = 0;
-                _positionMC.Y += _sensYMC * _vitesseMC * deltaTime;
+                _sensPersoY = 1;
+                _positionPerso.Y += _sensPersoY * _vitessePerso * deltaTime;
 
             }
             //######################################################
             //                      ANIMATION
             //######################################################
 
-            if (_sensXMC == 1 && _sensYMC == 0)
-                _MC.Play("right_Walk");
-            else if (_sensXMC == -1 && _sensYMC == 0)
-                _MC.Play("left_Walk");
-            else if (_sensYMC == -1 && _sensXMC == 0)
-                _MC.Play("up_Walk");
-            else if (_sensYMC == 1 && _sensXMC == 0)
-                _MC.Play("down_Walk");
-            if (_sensXMC == 0 && _sensYMC == 0)
-                _MC.Play("idle");
+            if (_sensPersoX == 1 && _sensPersoY == 1 || _sensPersoX == 1 && _sensPersoY == -1)
+                _Perso.Play("right_Walk");
+            else if (_sensPersoX == -1 && _sensPersoY == 1 || _sensPersoX == -1 && _sensPersoY == -1)
+                _Perso.Play("left_Walk");
+            else if (_sensPersoY == -1 && _sensPersoX == 0)
+                _Perso.Play("up_Walk");
+            else if (_sensPersoY == 1 && _sensPersoX == 0)
+                _Perso.Play("down_Walk");
+            
+            //######################################################
+            //                      ATTAQUE
+            //######################################################
+            if (_keyboardState.IsKeyDown(Keys.Space) && (_sensPersoX == 1 && _sensPersoY == 0))
+                _Perso.Play("right_swing");
+            if (_keyboardState.IsKeyDown(Keys.Space) && (_sensPersoX == -1 && _sensPersoY == 0))
+                _Perso.Play("left_swing");
+            if (_keyboardState.IsKeyDown(Keys.Space) && (_sensPersoY == 1 && _sensPersoX == 0))
+                _Perso.Play("down_swing");
+            if (_keyboardState.IsKeyDown(Keys.Space) && (_sensPersoY == -1 && _sensPersoX == 0))
+                _Perso.Play("up_swing");
+
+
 
             base.Update(gameTime);
         }
@@ -137,7 +146,7 @@ namespace SAE
             
             _tiledMapRenderer.Draw();
             _spriteBatch.Begin();
-            _spriteBatch.Draw(_MC, _positionMC);
+            _spriteBatch.Draw(_Perso, _positionPerso);
             _spriteBatch.End();
 
             base.Draw(gameTime);
