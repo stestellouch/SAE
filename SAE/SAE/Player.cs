752c39a88488c68e28dd869710b14fca1f6bab52
Stella Maestre <stella.maestre@etu.univ-smb.fr>
2023-01-05 09:28:35 +0100

    PLAYER

--- SAE/SAE/Player.cs
+++ SAE/SAE/Player.cs
@@ -1,20 +1,39 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using MonoGame.Extended.Content;
-using MonoGame.Extended.Serialization;
-using MonoGame.Extended.Sprites;
-using MonoGame.Extended.Tiled;
-using MonoGame.Extended.Tiled.Renderers;
 
 namespace SAE
 {
-    internal class Player
+    public class Player : Sprite
     {
+        public Player(Texture2D texture)
+          : base(texture)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            var velocity = new Vector2();
+
+            var speed = 3f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                velocity.Y = -speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.S))
+                velocity.Y = speed;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+                velocity.X = -speed;
+            else if (Keyboard.GetState().IsKeyDown(Keys.D))
+                velocity.X = speed;
+
+            Position += velocity;
+        }
     }
 }
