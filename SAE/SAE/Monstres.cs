787bc61823a2fdb6e04c8b1cd4b4678416016a59
Evan Lamy <evan.lamy@etu.univ-smb.fr>
2023-01-06 16:53:06 +0100

    Yes

--- SAE/SAE/Monstres.cs
+++ SAE/SAE/Monstres.cs
@@ -1,15 +1,15 @@
-﻿//using Microsoft.Xna.Framework;
-//using Microsoft.Xna.Framework.Graphics;
-//using Microsoft.Xna.Framework.Input;
-//using MonoGame.Extended.Content;
-//using MonoGame.Extended.Serialization;
-//using MonoGame.Extended.Sprites;
-//using MonoGame.Extended.Tiled;
-//using MonoGame.Extended.Tiled.Renderers;
-//using System.Collections.Generic;
-//using MonoGame.Extended;
-//using MonoGame.Extended.ViewportAdapters;
-//using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using System.Collections.Generic;
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
+using System;
 
 namespace SAE
 {
