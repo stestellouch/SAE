94f6c6a7706f3639b65910cdfcde155f60245c16
Evan Lamy <evan.lamy@etu.univ-smb.fr>
2023-01-11 15:38:13 +0100

    m

--- SAE/SAE/Enemies.cs
+++ SAE/SAE/Enemies.cs
@@ -22,35 +22,35 @@ namespace SAE
         public Rectangle collision;
 
         public bool isVisible = true;
-         
+
 
         public Enemies(Texture2D newTexture, Vector2 newPosition, int vie, bool enVie)
         {
             _textureEnemy = newTexture;
             _positionEnemy = newPosition;
-            
+
             _originWidth = (_textureEnemy.Width / 2);
             _originHeight = (_textureEnemy.Height / 2);
             _texture = newTexture;
             _position = newPosition;
             _vieMonstre = vie;
             _estEnVie = enVie;
-            
+
             originWidth = (_texture.Width / 2);
             originHeight = (_texture.Height / 2);
 
             collision = new Rectangle((int)this._position.X, (int)this._position.Y, 40, 25);
-            
+
         }
-        
-        
+
+
         public void Update(GraphicsDevice graphics, GameTime gameTime)
         {
 
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             float walkSpeed = (float)(deltaTime * (Perso._vitessePerso - 30));
             //Si le monstre est à droite du personnage
-            if (this._position.X  >= Perso._positionPerso.X && _estEnVie == true)
+            if (this._position.X >= Perso._positionPerso.X && _estEnVie == true)
             {
                 this._position.X -= walkSpeed;
                 //this.collision.X -= (int)walkSpeed;
@@ -62,7 +62,7 @@ namespace SAE
                 //this.collision.X += (int)walkSpeed;
             }
             //Si le monstre est au dessous du personnage
-            if (this._position.Y  >= Perso._positionPerso.Y && _estEnVie == true)
+            if (this._position.Y >= Perso._positionPerso.Y && _estEnVie == true)
             {
                 this._position.Y -= walkSpeed;
                 //this.collision.Y -= (int)walkSpeed;
@@ -79,7 +79,7 @@ namespace SAE
             ///                Degats                   ///
             ///////////////////////////////////////////////
             ///
-            Console.WriteLine(Perso.colisionPerso.X + " perso" +Perso.colisionPerso.Y);
+            Console.WriteLine(Perso.colisionPerso.X + " perso" + Perso.colisionPerso.Y);
             Console.WriteLine(this.collision.X + " fantome" + this.collision.Y);
             //if (Perso.colisionPerso.X<= this.collision.X && Perso.colisionPerso.X+64 >= this.collision.X 
             //    && Perso.colisionPerso.Y <= this.collision.Y && Perso.colisionPerso.Y +64 >= this.collision.Y)
@@ -129,18 +129,18 @@ namespace SAE
             //        Perso._viePerso -= 10;
             //    }
             //}
-            
-            if (this._vieMonstre <=0)
+
+            if (this._vieMonstre <= 0)
             {
                 _estEnVie = false;
             }
             //else if (Perso._viePerso <= 0)
             //{
 
-                //}
-            }
-           
+            //}
         }
+
+
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(_textureEnemy, _positionEnemy, Color.White);
@@ -149,7 +149,7 @@ namespace SAE
 
         }
 
-
-
     }
+
+    
 }
